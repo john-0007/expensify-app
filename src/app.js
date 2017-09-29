d223830f8646685a7764c71fb15a426d09f66e94
John <Xohnjoseph@gmail.com>
2017-09-29 22:14:53 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+ReactDOM.render(<p>This is a React boilerplate</p>,document.getElementById("app"))
