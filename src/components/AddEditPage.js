d0e000159dffcf58809c53263d05070d0a2920ef
John <Xohnjoseph@gmail.com>
2017-09-30 00:51:46 +0700

    add react-routers

---
+++ src/components/AddEditPage.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+
+const AddEditPage=()=>(
+  <div>
+    This is from my Edit component
+  </div>
+);
+
+export default  AddEditPage;
---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+
+const AddExpensePage=()=>(
+  <div>
+    This is from my Expense component
+  </div>
+);
+
+export default AddExpensePage;
